9f8a0488e9ccf4a579f1a827d7372542a12afefa
Felipe Niño <felipe.nino@api-ux.com>
2020-01-29 15:11:21 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -16,6 +16,91 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to this Docker Meetup with Jenkins
+
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to this Docker Meetup with Jenkins
+
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to this Docker Meetup with Jenkins
+
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to this Docker Meetup with Jenkins
+
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to this Docker Meetup with Jenkins
+
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to this Docker Meetup with Jenkins
+
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to this Docker Meetup with Jenkins
+
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
